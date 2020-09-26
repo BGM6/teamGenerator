cd3316e5999ab5ed65cbf8fb5be59d28cae1839d
bguilas <bryanjguilas@gmail.com>
2020-09-26 08:19:59 -0700

    code added to app.js

--- app.js
+++ app.js
@@ -1,14 +1,138 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+const render = require("./lib/htmlRenderer");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// Employee 
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+const teamMembers = [];
+let manager;
+let teamTitle;
+
+// Manager inquirer function and questions
+function managerData() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "Please provide your teams name: ",
+            name: "teamTitle"
+        },
+        {
+            type: "input",
+            message: "Who is the manager of this project/team?",
+            name: "managerName"
+        },
+        {
+            type: "input",
+            message: "Please provide the managers ID: ",
+            name: "managerID"
+        },
+        {
+            type: "input",
+            message: "Please provide the managers email address: ",
+            name: "managerEmail"
+        },
+        {
+            type: "input",
+            message: "Please provide the managers office number: ",
+            name: "officeNumber"
+        }]).then(managerResponse => {
+            manager = new Manager(managerResponse.managerName, managerResponse.managerID, managerResponse.managerEmail, managerResponse.officeNumber);
+            teamTitle = managerResponse.teamTitle;
+            secondaryEmployeeData();
+        });
+}
+
+// Secondary employee information getter
+function secondaryEmployeeData() {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What is this employee's role?",
+            name: "employeeRole",
+            choices: ["Intern", "Engineer"]
+        },
+        {
+            type: "input",
+            message: "What is the employee's name?",
+            name: "employeeName"
+        },
+        {
+            type: "input",
+            message: "What is the employee's id?",
+            name: "employeeId"
+        },
+        {
+            type: "input",
+            message: "What is the employee's email?",
+            name: "employeeEmail"
+        },
+        {
+            type: "input",
+            message: "What is the Engineer's Github?",
+            name: "github",
+            when: (userInput) => userInput.employeeRole === "Engineer".toLowerCase()
+        },
+        {
+            type: "input",
+            message: "What is the name of the school attended by the intern?",
+            name: "school",
+            when: (userInput) => userInput.employeeRole === "Intern".toLowerCase()
+        },
+        {
+            type: "confirm",
+            name: "newEmployee",
+            message: "Would you like to add another team member (yes/no)?" 
+        }
+    ]).then(answers => {
+
+        //============================================================
+        // Pushes a new intern into the team members array
+        //============================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
